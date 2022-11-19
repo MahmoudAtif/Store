40a5f020d40943a1fdda8538599d5071456e78eb
Mahmoud Atif <mahmoudateff07@gmail.com>
2022-11-19 17:56:31 +0200

    requirments changes

--- user/urls.py
+++ user/urls.py
@@ -6,11 +6,11 @@ from rest_framework.routers import DefaultRouter
 from rest_framework.authtoken.views import obtain_auth_token
 from django.urls import re_path
 
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-    TokenVerifyView
-)
+# from rest_framework_simplejwt.views import (
+#     TokenObtainPairView,
+#     TokenRefreshView,
+#     TokenVerifyView
+# )
 
 
 
@@ -54,9 +54,9 @@ urlpatterns = [
     path('registerApiView/', apiViews.RegisterApiView.as_view(),name='register'),
 
     #######JWT(JSON WEB TOKEN)
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/token/verify' , TokenVerifyView.as_view() ,name='token_verify'),
+    # path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    # path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    # path('api/token/verify' , TokenVerifyView.as_view() ,name='token_verify'),
 
 
 
